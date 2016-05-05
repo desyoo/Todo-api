e5dd6dfeaea4b1989a94f4aca767b7e1ca5dd7ca
Daniel Yoo <daneyoo@gmail.com>
2016-05-05 10:06:05 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Daniel',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Daniel',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+
+var myArray = [3,2];
+
+function updateArray(array) {
+	// array = [3, 3];
+	array.push(33);
+	debugger;
+}
+
+updateArray(myArray);
+console.log(myArray);
